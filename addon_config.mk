adca19f7cd263554feeb683f44702fbd95016559
LeoColomb <leocolombaro@yahoo.fr>
2013-08-03 18:03:09 +0200

    Move install.xml intoi addon_config.mk

---
+++ addon_config.mk
@@ -0,0 +1,6 @@
+﻿meta:
+	ADDON_NAME = ofxXively
+	ADDON_DESCRIPTION = openFrameworks addon for talking to Xively
+	ADDON_AUTHOR = Leo Colombaro
+	ADDON_TAGS = "xively" "data" "api"
+	ADDON_URL = https://github.com/LeoColomb/ofxXively
